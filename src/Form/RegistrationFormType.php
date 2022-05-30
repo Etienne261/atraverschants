c21767efa7342d47c81fb56ef36b4b6bc325a424
Etienne261 <93247779+Etienne261@users.noreply.github.com>
2022-05-30 14:04:40 +0200

    inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,16 +11,61 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('phone')
+            ->add(
+                'lastname',
+                TextType::class,
+                ['label' => 'Nom *'],
+                ['required' => true]
+            )
+            ->add(
+                'firstname',
+                TextType::class,
+                ['label' => 'Prénom *'],
+                ['required' => true]
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                ['label' => 'Email *'],
+                ['required' => true]
+            )
+            ->add(
+                'phone',
+                TelType::class,
+                ['label' => 'Téléphone *'],
+                ['required' => true]
+            )
+            ->add(
+                'plainPassword',
+                PasswordType::class,
+                [ // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'mapped' => false,
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Merci d\'entrer un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit contenir {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+            )
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'label' => 'Acceptez les conditions',
@@ -30,24 +75,12 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Merci d\'entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            ->add(
+                'save',
+                SubmitType::class,
+                ['label' => 'Valider'],
+                ['attr' => ['class' => 'save']]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
